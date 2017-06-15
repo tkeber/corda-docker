b9d90598b53f082310806533867dfcd746761c38
Wawrzyniec Niewodniczański <wawrzek@r3.com>
2017-06-15 16:23:33 +0100

    Improve handling variables in shell
    ${parameter:-word} rather than ${parameter-word} for most of parameters
    to avoid null.

--- corda-0.11.1.sh
+++ corda-0.11.1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export CORDA_HOST="${CORDA_HOST-localhost}"
-export CORDA_PORT_P2P="${CORDA_PORT-10002}"
-export CORDA_PORT_RPC="${CORDA_PORT-10003}"
-export CORDA_LEGAL_NAME="${CORDA_LEGAL_NAME-Corda Test Node}"
-export CORDA_ORG="${CORDA_ORG-CordaTest}"
-export CORDA_ORG_UNIT="${CORDA_ORG_UNIT-CordaTest}"
-export CORDA_COUNTRY="${CORDA_COUNTRY}-UK"
-export CORDA_CITY="${CORDA_CITY-London}"
-export CORDA_EMAIL="${CORDA_EMAIL-admin@corda.test}"
+export CORDA_HOST="${CORDA_HOST:-localhost}"
+export CORDA_PORT_P2P="${CORDA_PORT:-10002}"
+export CORDA_PORT_RPC="${CORDA_PORT:-10003}"
+export CORDA_LEGAL_NAME="${CORDA_LEGAL_NAME:-Corda Test Node}"
+export CORDA_ORG="${CORDA_ORG:-CordaTest}"
+export CORDA_ORG_UNIT="${CORDA_ORG_UNIT:-CordaTest}"
+export CORDA_COUNTRY="${CORDA_COUNTRY:-UK}"
+export CORDA_CITY="${CORDA_CITY:-London}"
+export CORDA_EMAIL="${CORDA_EMAIL:-admin@corda.test}"
 export JAVA_OPTIONS="${JAVA_OPTIONS--Xmx512m}"
 export JAVA_CAPSULE="${JAVA_CAPSULE-''}"
 
