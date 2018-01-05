aa171389c8c768780ddb3675de3aeeebefeb1af2
Przemyslaw Bak <p.bak@r3.com>
2018-01-05 08:29:11 +0000

    Start webserver along with corda jar

--- files/run-corda.sh
+++ files/run-corda.sh
@@ -4,4 +4,5 @@ export JAVA_OPTIONS="${JAVA_OPTIONS--Xmx512m}"
 export CORDA_HOME=/opt/corda
 
 cd ${CORDA_HOME}
+java $JAVA_OPTIONS -jar ${CORDA_HOME}/corda-webserver.jar 2>&1 &
 java $JAVA_OPTIONS -jar ${CORDA_HOME}/corda.jar 2>&1
